13ec9aaf0d3e14bc6329abc5f73e99a681378c56
jimmjy <jfinkelstein9@me.com>
2025-02-08 19:08:27 -0500

    did some exercises for section 1

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,16 @@
+"""
+    A rubber ball is dropped from a height of 100 meters and each time it hits the gorund,
+    it bonces back up to 3/5 the height it fell.
+    
+    Write a program that prints a table showing the height of the first 10 bounces.
+"""
+
 # bounce.py
 #
 # Exercise 1.5
+drop_height = 100
+bounce_factor = 3 / 5
+
+for i in range(10):
+    drop_height *= bounce_factor
+    print(round(drop_height, 4))
--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,7 @@
 # fileparse.py
 #
 # Exercise 3.3
+import urllib.request
+u = urllib.request.urlopen('https://flexiple.com/python/print-object-attributes-python')
+data = u.read()
+print(data)
